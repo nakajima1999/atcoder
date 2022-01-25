724b0cd22e37901b9839d587bb1c1a36bc940ef7
nakajima1999 <s1711547@klis.tsukuba.ac.jp>
2022-01-25 23:53:25 +0900

    アルゴリズムと数学 演習問題集(2022/1/25)

--- alg_math014.py
+++ alg_math014.py
@@ -2,16 +2,16 @@
 # 014 - Factorization
 def factorization(n):
     arr = []
-    temp = n
-    for i in range(2, int(-(-n**0.5//1))+1):
-        if temp % i==0:
-            while temp % i== 0:
+    temp = n  # nを素数で割っていく
+    for i in range(2, int(-(-n ** 0.5 // 1)) + 1):
+        if temp % i == 0:  # iで割り切れる場合
+            while temp % i == 0:  # tempをiで割れるだけ割る
                 temp //= i
                 arr.append(str(i))
-
-    if temp != 1:
-        arr.append(str(temp))
-    if arr == []:
+    
+    if temp != 1:  # 最終的なtempが1以外の場合，素数であるため追加
+       arr.append(str(temp))
+    if arr == []:  # arrがからの場合，n自体が素数であるため追加
         arr.append(str(n))
     return arr
 
