1dca6b6559b84659cbaa75d6f85435969072cd24
nakajima1999 <s1711547@klis.tsukuba.ac.jp>
2022-01-27 11:17:37 +0900

    アルゴリズムと数学 演習問題集(2022/1/27)

--- alg_math016.py
+++ alg_math016.py
@@ -5,3 +5,12 @@ from functools import reduce
 n = int(input())
 a = list(map(int, input().split()))
 print(reduce(math.gcd, a))
+
+'''
+メモ
+- functools モジュール: 高階関数
+    - 関数に影響を及ぼしたり，他の関数を返したりする関数のためのもの
+- functools.reduce(function, iterable[, initializer])
+    - iterable の要素に対して、iterable を単一の値に短縮するような形で 
+      2 つの引数をもつ function を左から右に累積的に適用
+'''
